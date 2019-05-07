c7222c4d3e82195e67e153ec3bf4b603a61e1a29
clintgitahi <clinton@yumkenya.com>
2019-05-07 17:27:25 +0300

    landing page styles

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import { View, Button } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 
 export default class Landing extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
   constructor(props) {
     super(props);
 
@@ -20,12 +24,37 @@ export default class Landing extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
-      <View>
-        <Button title="Login" onPress={this.onLoginPress} />
-        <Button title="Sign Up" onPress={this.onRegisterPress} />
+      <View style={styles.container}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            bottom: 20,
+            left: 0,
+            position: "absolute"
+          }}
+        >
+          <View style={{ flex: 4 }}>
+            <Button title="Login" onPress={this.onLoginPress} color="#06b7cc" />
+          </View>
+
+          <View style={{ flex: 4 }}>
+            <Button
+              title="Sign Up"
+              onPress={this.onRegisterPress}
+              color="#fb9b03"
+            />
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff"
+  }
+});
