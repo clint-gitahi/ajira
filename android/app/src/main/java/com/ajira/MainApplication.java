2e8dece1cc75d97ef3742b7f414c97bd7ca60435
clintgitahi <clinton@yumkenya.com>
2019-05-07 11:22:56 +0300

    adding react native gesture

--- android/app/src/main/java/com/ajira/MainApplication.java
+++ android/app/src/main/java/com/ajira/MainApplication.java
@@ -3,7 +3,9 @@ package com.ajira;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage()
+            new RNGestureHandlerPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
